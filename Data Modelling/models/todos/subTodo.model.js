a5bb2415478c4e193111ee7b3208b19ad623be69
animesh7780 <thakuranimesh235@gmail.com>
2024-10-19 20:14:33 +0530

    Data Modelling

--- Data Modelling/models/todos/subTodo.model.js
+++ Data Modelling/models/todos/subTodo.model.js
@@ -1,5 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({})
+const subTodoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    todoId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Todo'
+    }
+}, { timestamps: true })
 
-export const User = mongoose.model('User', userSchema)
+export const SubTodo = mongoose.model('subTodo', subTodoSchema)
--- Data Modelling/models/todos/todo.model.js
+++ Data Modelling/models/todos/todo.model.js
@@ -1,5 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({})
+const todoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdby: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    subtodos: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'subTodo'
+    }]//array of subTodos
+}, { timestamps: true })
 
-export const User = mongoose.model('User', userSchema)
+export const User = mongoose.model('todo', todoSchema)
