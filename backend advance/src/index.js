fec4ab3df4955710b2151519da775f4e6db73db9
animesh7780 <thakuranimesh235@gmail.com>
2024-10-23 11:00:11 +0530

    update

--- backend advance/src/index.js
+++ backend advance/src/index.js
@@ -7,12 +7,16 @@ dotenv.config();
 
 const app = express();
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 
 
 
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port", process.env.PORT);
-
-})
